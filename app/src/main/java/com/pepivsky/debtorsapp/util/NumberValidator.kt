332abc29a71355a38101be0a2f29a8e0686616bb
pepivsky <ironglas68@gmail.com>
2024-04-08 16:13:49 -0600

    added comment

--- app/src/main/java/com/pepivsky/debtorsapp/util/NumberValidator.kt
+++ app/src/main/java/com/pepivsky/debtorsapp/util/NumberValidator.kt
@@ -4,5 +4,6 @@ import android.util.Log
 
 fun numberValidator(input: String): Boolean {
     val countDots = input.count {it == '.' } < 2
+    // acept 0..9 and '.'
     return input.all { it in '\u0030'..'\u0039' || it == '.' } && countDots
 }
