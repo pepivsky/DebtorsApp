40f53dde74d866e209ab2303ebcf9b72c4a819ea
pepivsky <ironglas68@gmail.com>
2024-04-11 15:07:48 -0600

    using font in typography

--- app/src/main/java/com/pepivsky/debtorsapp/ui/theme/Type.kt
+++ app/src/main/java/com/pepivsky/debtorsapp/ui/theme/Type.kt
@@ -2,9 +2,23 @@ package com.pepivsky.debtorsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pepivsky.debtorsapp.R
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -12,32 +26,37 @@ val Typography = Typography(
         fontWeight = FontWeight.SemiBold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        fontFamily = poppinsFamily,
     ),
     titleLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp,
         lineHeight = 32.sp,
-        letterSpacing = 0.0.sp
-    ),
+        letterSpacing = 0.0.sp,
+        fontFamily = poppinsFamily,
+        ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.15.sp
-    ),
+        letterSpacing = 0.15.sp,
+        fontFamily = poppinsFamily,
+        ),
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
-    ),
+        letterSpacing = 0.25.sp,
+        fontFamily = poppinsFamily,
+        ),
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
+        letterSpacing = 0.5.sp,
+        fontFamily = poppinsFamily,
+        )
 )
     /* Other default text styles to override
     titleLarge = TextStyle(
