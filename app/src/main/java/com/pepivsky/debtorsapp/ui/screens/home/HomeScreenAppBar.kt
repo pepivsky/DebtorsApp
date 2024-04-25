02e662c8f99782e1ea500323008065666076f593
pepivsky <ironglas68@gmail.com>
2024-04-25 12:07:03 -0600

    changed font size

--- app/src/main/java/com/pepivsky/debtorsapp/ui/screens/home/HomeScreenAppBar.kt
+++ app/src/main/java/com/pepivsky/debtorsapp/ui/screens/home/HomeScreenAppBar.kt
@@ -1,23 +1,26 @@
 package com.pepivsky.debtorsapp.ui.screens.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.pepivsky.debtorsapp.navigation.AppScreens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeAppBar(navController: NavController) {
-    CenterAlignedTopAppBar(title = { Text(text = "Deudores") },
+    CenterAlignedTopAppBar(title = { Text(text = "Deudores", fontSize = 24.sp) },
         actions = {
             IconButton(onClick = {
                 navController.navigate(
