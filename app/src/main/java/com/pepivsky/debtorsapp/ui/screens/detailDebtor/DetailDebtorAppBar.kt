a37fe34f1a88657f589426bf61aa76c4c770c00d
pepivsky <ironglas68@gmail.com>
2024-06-07 17:44:09 -0600

    file name modified

--- app/src/main/java/com/pepivsky/debtorsapp/ui/screens/detailDebtor/DetailDebtorAppBar.kt
+++ app/src/main/java/com/pepivsky/debtorsapp/ui/screens/detailDebtor/DetailDebtorAppBar.kt
@@ -140,7 +140,7 @@ fun DetailDebtorAppBarActions(
         onDelete = { showDialogConfirmDelete = true },
         onEdit = { onEditClicked() },
         onGeneratePDF = {
-            launcher.launch("hola.pdf")
+            launcher.launch("detalle_deuda_${debtorWithMovements.debtor.name}.pdf")
         }
     )
 }
